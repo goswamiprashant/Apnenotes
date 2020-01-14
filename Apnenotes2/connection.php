95a4a767db60786cae3bede0e29c8e4242042204
goswamiprashant <48816540+goswamiprashant@users.noreply.github.com>
2020-01-14 14:07:44 +0530

    Add files via upload

---
+++ Apnenotes2/connection.php
@@ -0,0 +1,8 @@
+<?php 
+$con=mysqli_connect('sql113.byetcluster.com','epiz_23731967',
+'p40Zx8H2acgP','epiz_23731967_suggestions');
+if(!$con)
+{
+die ('Please check your connection'.mysqli_error($con));
+}
+?>
---
+++ Apnenotes2/index.php
@@ -0,0 +1,442 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<title>Apnenotes</title>
+<meta name="description" content="It is simple source through which student 
+can get enginnering notes easliy and systematically."/>
+<meta name="keywords" content="Engineering notes,notes, notes of enginnering "/>
+<meta  name="author" content="Prashant Goswami"/> 
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/6
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+    <meta name="viewport" content="width=device-width"/>
+
+<link rel="stylesheet" type="text/css" href="./css/style.css" />
+<style>
+input[type=text], select, textarea{
+  width: 100%;
+  padding: 12px;
+  border: 1px solid rgb(113,0,0);
+  border-radius: 4px;
+  box-sizing: border-box;
+  resize: vertical;
+}
+
+
+label {
+  padding: 12px 12px 12px 0;
+  display: inline-block;
+}
+
+
+input[type=submit] {
+  background-color: rgb(228,228,228);
+  color: black;
+  padding: 12px 12px;
+  border: 1px solid black;
+  border-radius: 4px;
+  cursor: pointer;
+  float: right;
+  
+  
+  
+}
+
+.layout {
+  
+  border-radius: 5px;
+  background-color: #c4dfe6;
+  padding: 12px;
+  margin-right:20%;
+  margin-left:20%;
+  box-shadow:3px 3px 2px 2px black;
+}
+
+/* Floating column for labels: 25% width */
+.col-25 {
+  float: left;
+  width: 25%;
+  margin-top: 2px;
+}
+.col-75 {
+  float: left;
+  width: 75%;
+  margin-top: 2px;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+
+  .col-25, .col-75, input[type=submit], p.para1 {
+    width: 100%;
+    margin-top: 0;
+  }
+
+p.para1{
+font-family:"cooper black";
+font-style:bold;
+color:black;
+background-color: rgb(255,153,104);
+border-style:groove;
+border-color:black;
+border-width:2px;
+margin-right:400px;
+line-height:1.5em;
+HEIGHT:50PX;
+cursor:crosshair;
+}
+
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+button:hover {
+  opacity: 0.8;
+}
+.can_btn {
+  width: auto;
+  padding: 10px 18px;
+  background-color: #f44336;
+}
+.class1{
+  padding: 16px;
+ text-align: center;
+}
+span.psw {
+  float: right;
+  padding-top: 16px;
+}
+  span.psw {
+    display: block;
+    float: none;
+  }
+ 
+.layout1{
+margin-left:20%;
+margin-right:20%;
+}
+ 
+
+</style>
+</head>
+<body>
+
+
+<header style="position:fixed;z-index:100;width:100%;
+border-bottom:10px ridge brown;">
+<div class="heading">
+<div <h1><span class="highlight"><img src="./images/logo2.PNG" height="100px"
+ width="250px" alt="mylogo.png" />
+ </span></h1>
+<p></p>  
+</div>
+<nav >
+<ul>
+<li><a href="#" class="current">Home</a>
+</li>
+<li><a href="notes.html">Notes</a></li>
+<li><a href="about_us.html">About us </a>
+</li>
+
+</ul>
+</nav>
+</div>
+</header>
+
+
+<section style="position:relative;top:130px;">
+
+<section id="mid_1">
+<img  height="600px" width="100%" src="./images/img5.jpg" alt="courtsey:Apne-notes"/>
+<div class="mid_section">
+<div class="tagline">
+<span class="highlight">Aasan</span> Enginnering Notes
+<p> EK kadam Ujawal bhavisya ki aur.......</p></div>
+<p style=" font-size:30px;margin-left:auto;margin-right:auto;padding:30px;
+width:80%;height:170px;color:(0,0,0,0.9);
+text-indent:40px;background-color:white;box-shadow:3px 3px gray;"> Here we are trying to provide simple ,easy understable, notes to the students which 
+covers all the syllabus in a very efficient way.Actually we can understand the difficulties 
+arises during the study time they are proved to be very helpful in erdicating all your diffficulties.</p>
+</div>
+</section>
+
+<section class="bgwhite">
+<section style="position:relative;bottom:100px;" class="year_sec">
+<div  class="year">
+<img width="100%" src="./images/img9.jpg" alt="courtsey:apne-notes"/>
+
+<div class="font">
+<span style="filter:blur(0px);
+-webkit-filter:blur(0px);"><a href=".\Firstyear\1styear.html" style="font-size:50px;">First Year</a></span>
+<span><a style="font-size:50px;" href="2ndyear_branches.html">Second Year</a></span>
+<span><a style="font-size:50px;"href="3rdyear_branches.html">Third Year</a></span>
+<span><a style="font-size:50px;"href="4thyear_branches.html">Fourth Year</a></span></div>
+</div>
+</section>
+</section>
+
+
+<section class="bgwhite">
+<section class="hand_writtennotes_sec">
+<div class="mid_4">
+<img src="./images/img52.jpg" alt="courtsey:apne notes"/>
+<span>Lecture Notes<div style="font-size:20px;">Haato ki mehnat</div></span>
+<div style="background-color:rgba(0,0,0,0.4);margin:auto;
+display:block;color:white;
+height:100px;	
+width:80%;position:relative;bottom:400px;font-size:24px;
+margin-left:auto;margin-right:auto;padding:30px;
+color:white;
+text-shadow:4px 0px 2px black;
+box-shadow:3px 3px white;">
+
+<p>The hand written notes are best means of learning beacuse we can connect ourself to it internally,
+it create an anthor ind of feel during studyiing through it. 
+</p>
+</div>
+</div>
+</section>
+</section>
+
+
+<section class="bgwhite">
+<section class="hand_writtennotes_sec">
+<div class="mid_4">
+<img src="./images/img44.jpg" alt="courtsey:apne notes"/>
+<span>Lecture videos<div style="font-size:20px;">Haato ki mehnat</div></span>
+<div style="background-color:rgba(0,0,0,0.4);margin:auto;
+display:block;color:white;
+height:100px;	
+width:80%;position:relative;bottom:400px;font-size:24px;
+margin-left:auto;margin-right:auto;padding:30px;
+color:white;
+text-shadow:4px 0px 2px black;
+box-shadow:3px 3px white;">
+
+<p>The hand written notes are best means of learning beacuse we can connect ourself to it internally,
+it create an anthor ind of feel during studyiing through it. 
+</p>
+</div>
+</div>
+</section>
+</section>
+
+
+<section class="bgwhite">
+<section class="hand_writtennotes_sec">
+<div class="mid_4">
+<img src="./images/img45.jpg" alt="courtsey:apne notes"/>
+<span>Short Notes<div style="font-size:20px;">Haato ki mehnat</div></span>
+<div style="background-color:rgba(0,0,0,0.4);margin:auto;
+display:block;color:white;
+height:100px;	
+width:80%;position:relative;bottom:400px;font-size:24px;
+margin-left:auto;margin-right:auto;padding:30px;
+color:white;
+text-shadow:4px 0px 2px black;
+box-shadow:3px 3px white;">
+
+<p>The hand written notes are best means of learning beacuse we can connect ourself to it internally,
+it create an anthor ind of feel during studyiing through it. 
+</p>
+</div>
+</div>
+</section>
+</section>
+
+
+<section class="bgwhite">
+<section class="hand_writtennotes_sec">
+<div class="mid_4">
+<img src="./images/img47.jpg" alt="courtsey:apne notes"/>
+<span>Assignments<div style="font-size:20px;">Haato ki mehnat</div></span>
+<div style="background-color:rgba(0,0,0,0.4);margin:auto;
+display:block;color:white;
+height:100px;	
+width:80%;position:relative;bottom:400px;font-size:24px;
+margin-left:auto;margin-right:auto;padding:30px;
+color:white;
+text-shadow:4px 0px 2px black;
+box-shadow:3px 3px white;">
+
+<p>The hand written notes are best means of learning beacuse we can connect ourself to it internally,
+it create an anthor ind of feel during studyiing through it. 
+</p>
+</div>
+</div>
+</section>
+</section>
+
+
+<section class="bgwhite">
+<section class="hand_writtennotes_sec">
+<div class="mid_4">
+<img src="./images/img48.jpg" alt="courtsey:apne notes"/>
+<span>Practical Files<div style="font-size:20px;">Haato ki mehnat</div></span>
+<div style="background-color:rgba(0,0,0,0.4);margin:auto;
+display:block;color:white;
+height:100px;	
+width:80%;position:relative;bottom:400px;font-size:24px;
+margin-left:auto;margin-right:auto;padding:30px;
+color:white;
+text-shadow:4px 0px 2px black;
+box-shadow:3px 3px white;">
+
+<p>The hand written notes are best means of learning beacuse we can connect ourself to it internally,
+it create an anthor ind of feel during studyiing through it. 
+</p>
+</div>
+</div>
+</section>
+</section>
+
+
+<section class="bgwhite">
+<section class="hand_writtennotes_sec">
+<div class="mid_4">
+<img src="./images/img50.jpg" alt="courtsey:apne notes"/>
+<span>Practical Videos<div style="font-size:20px;">Haato ki mehnat</div></span>
+<div style="background-color:rgba(0,0,0,0.4);margin:auto;
+display:block;color:white;
+height:100px;	
+width:80%;position:relative;bottom:400px;font-size:24px;
+margin-left:auto;margin-right:auto;padding:30px;
+color:white;
+text-shadow:4px 0px 2px black;
+box-shadow:3px 3px white;">
+
+<p>The hand written notes are best means of learning beacuse we can connect ourself to it internally,
+it create an anthor ind of feel during studyiing through it. 
+</p>
+</div>
+</div>
+</section>
+</section>
+
+
+<section class="bgwhite">
+<section class="hand_writtennotes_sec">
+<div class="mid_4">
+<img src="./images/img49.jpg" alt="courtsey:apne notes"/>
+<span>Conclusive videos<div style="font-size:20px;">Haato ki mehnat</div></span>
+<div style="background-color:rgba(0,0,0,0.4);margin:auto;
+display:block;color:white;
+height:100px;	
+width:80%;position:relative;bottom:400px;font-size:24px;
+margin-left:auto;margin-right:auto;padding:30px;
+color:white;
+text-shadow:4px 0px 2px black;
+box-shadow:3px 3px white;">
+
+<p>The hand written notes are best means of learning beacuse we can connect ourself to it internally,
+it create an anthor ind of feel during studyiing through it. 
+</p>
+</div>
+</div>
+</section>
+</section>
+
+<!--
+<section class="bgwhite1" style="background-color:red;height:300px;margin-top:40px;">
+<form style=" font-size:30px;padding-top:40px;margin-left:auto;
+	margin-right:auto;
+	width:80%;"action="index.php" method="post">
+
+<fieldset>
+<legend>Suggestions</legend>
+<input style="width:700px;height:30px;font-size:20px;"type="text" name="name" placeholder="Enter your name"  required/><br/>
+<input style="width:700px;height:30px;font-size:20px;" type="email" name="email" placeholder="Email"  required /><br/>
+<input style="width:700px;height:30px;font-size:20px;" type="text" name="message" placeholder="Message" required/><br/>
+<input style="width:100px;height:25px;font-size:20px;font-weight:bold;background-color:gray;"type="submit" value="Send"/><br/>
+</fieldset>
+</section>  -->
+
+<br/><br/><br/><br/><br/><br/><br/>
+<div style="padding:30px;box-shadow:3px 3px 2px 2px gray;" class="layout">
+  
+
+<form style="margin-left:auto;margin-right:auto;width:80%;"action="index.php" method="post">
+
+ <fieldset>
+<legend> <h2 style="text-align:center;><font="arial" color="blue">Print Your Idea
+</font></h2></legend>
+<div style="padding:20px;">
+    <div class="row">
+      <div class="col-25">
+      
+       <label style="height:10px;" for="name">Name</label>
+        </div>
+         <div class="col-75">
+          
+        <input height="10px"  type="text" id="name" name="name" placeholder="Your name..">
+      </div>
+    </div>
+   
+<div class="row">
+<div class="col-25">
+        <label style="height:10px;" for="email">Email</label>
+      </div>
+ <div class="col-75">
+<input height="15px" type="text" name="email" placeholder="Email"  >
+</div></div>
+<div class="row">
+      <div class="col-25">
+        <label style="height:15px;" for="comments">Suggestions and Comments</label>
+      </div>
+      <div class="col-75">
+        <textarea style="height:60px;" id="subject" name="message" placeholder="Write your views here..." style="height:200px"></textarea>
+      </div>
+    </div>
+<br>
+    <div class="row">
+      <input style="height:40px;width:15%;"align="center" type="submit" value="Submit" >
+    </div>
+    </div>
+  </form>
+</div>
+
+</fieldset>
+
+
+<?php
+$con=mysqli_connect('sql113.byetcluster.com','epiz_23731967',
+'p40Zx8H2acgP','epiz_23731967_suggestions');
+
+$name=$_POST['name'];
+ $email=$_POST['email'];
+$message=$_POST['message'];
+$q="insert into suggestions(name,email,message) values('$name','$email','$message' )";
+mysqli_query($con,$q);
+mysqli_close($con);
+  ?>
+
+
+
+ 
+<footer >
+<pre class="footer_sec">
+Created and Designed by Prashant Goswami
+Apne Notes,Copyright &copy;2019
+</pre>
+</footer>
+
+</section>
+</body>
+</html>
